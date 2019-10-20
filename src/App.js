eaaabdd7aa3c7b108fa98660c227d0bbdeb22b57
Jereme Webb <dbzgtsaver@sbcglobal.net>
2019-10-20 11:59:20 -0700

    colorhelper updated

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+
+
 export default class Palette extends Component {
   render() {
     const colorBoxes = this.props.colors.map(color => (
