6e7f7cfc049f9f430624edbd719ecaa81831cace
Jereme Webb <dbzgtsaver@sbcglobal.net>
2020-04-28 10:52:13 -0700

    Updates

---
+++ precache-manifest.804974426a350ad7dfd0820fb5c88626.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69762274ba3cb293b9d968599ae71031",
+    "url": "/ColorPalette/index.html"
+  },
+  {
+    "revision": "639c8137b865b2e36543",
+    "url": "/ColorPalette/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "412104408220f88fe9cb",
+    "url": "/ColorPalette/static/css/main.bf284187.chunk.css"
+  },
+  {
+    "revision": "639c8137b865b2e36543",
+    "url": "/ColorPalette/static/js/2.334f81ae.chunk.js"
+  },
+  {
+    "revision": "412104408220f88fe9cb",
+    "url": "/ColorPalette/static/js/main.10581b4d.chunk.js"
+  },
+  {
+    "revision": "6c9ee018dce751fa9f1a",
+    "url": "/ColorPalette/static/js/runtime-main.5a6ec1c1.js"
+  },
+  {
+    "revision": "9819204481cd25f6f056debf6e084d02",
+    "url": "/ColorPalette/static/media/bg.98192044.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ColorPalette/static/media/logo.25bf045c.svg"
+  }
+]);
